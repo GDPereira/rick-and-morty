8a508ca3ebb4eed6b85240ba3e28bdc017e5b914
GDPereira <gdelucapereira@gmail.com>
2024-03-04 00:06:49 -0300

    feat: add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["standard-with-typescript", "plugin:react/recommended"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "@typescript-eslint/quotes": [
+      "error",
+      "double",
+      { allowTemplateLiterals: true },
+    ],
+    "@typescript-eslint/comma-dangle": ["error", "always-multiline"],
+    semi: "off",
+    "@typescript-eslint/semi": ["error", "always"],
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": [
+      1,
+      { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+    ],
+    "@typescript-eslint/strict-boolean-expressions": "off",
+  },
+};
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,8 +4,8 @@ import { ListCharacters } from "@components/molecules/ListCharacters";
 import { useCharacters } from "@hooks/useCharacters";
 import { Card, Input } from "@rneui/themed";
 import { useFilterStore } from "@zustand/filter";
-import { SafeAreaView } from "react-native-safe-area-context";
 import { debounce } from "lodash";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export const Home = () => {
   const { results, loading, error, getMoreData } = useCharacters();
@@ -15,10 +15,7 @@ export const Home = () => {
     <SafeAreaView style={{ flexGrow: 1 }}>
       <Card containerStyle={{ flex: 1 }}>
         <Card.Title h4>Rick and Morty Characters</Card.Title>
-        <Input
-          placeholder={"Filter"}
-          onChangeText={debounce(setName, 500)}
-        />
+        <Input placeholder={"Filter"} onChangeText={debounce(setName, 500)} />
         {loading && <Loading />}
         {error && <ErrorText />}
         {!loading && !error && (
